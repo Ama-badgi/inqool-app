77469651a5c0243ff5514e51a3f9cc0acc2abef3
xdobrock <xdobrock@fi.muni.cz>
2025-06-23 01:22:56 +0200

    feat: change the users table to react table

--- src/components/Users/index.tsx
+++ src/components/Users/index.tsx
@@ -1,33 +1,77 @@
 import { useFetchUsers } from "../../hooks/useUsers";
+import {
+  createColumnHelper,
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
 import { Link } from "react-router";
+import type { User } from "../../types/user";
+
+const columnHelper = createColumnHelper<User>();
+
+const columns = [
+  columnHelper.accessor("id", {
+    header: "ID",
+    footer: (info) => info.column.id,
+  }),
+  columnHelper.accessor("name", {
+    header: "Name",
+    footer: (info) => info.column.id,
+  }),
+  columnHelper.accessor("gender", {
+    header: "Gender",
+    footer: (info) => info.column.id,
+  }),
+  columnHelper.accessor("banned", {
+    header: "Banned",
+    cell: (info) => (info.getValue() ? "✅ Yes" : "❌ No"),
+    footer: (info) => info.column.id,
+  }),
+];
 
 function Users() {
-  const { isFetching, isError, error, data: users } = useFetchUsers();
+  const { data: users = [], isFetching, isError, error } = useFetchUsers();
 
-  if (isError) return <>Error: {error}</>;
+  const table = useReactTable({
+    data: users,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  });
+
   if (isFetching) return <>Loading...</>;
-  if (!users || users.length === 0) return <p>No users found.</p>;
+  if (isError) return <>Error: {error.message}</>;
 
   return (
     <>
+      {" "}
       <Link to="/">Home</Link>
       <h1>Users</h1>
-      <table className="min-w-full border border-gray-300">
-        <thead className="bg-gray-100">
-          <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Gender</th>
-            <th>Banned</th>
-          </tr>
+      <table>
+        <thead>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <tr key={headerGroup.id}>
+              {headerGroup.headers.map((header) => (
+                <th key={header.id}>
+                  {header.isPlaceholder
+                    ? null
+                    : flexRender(
+                        header.column.columnDef.header,
+                        header.getContext()
+                      )}
+                </th>
+              ))}
+            </tr>
+          ))}
         </thead>
         <tbody>
-          {users.map((user) => (
-            <tr key={user.id}>
-              <td>{user.id}</td>
-              <td>{user.name}</td>
-              <td>{user.gender}</td>
-              <td>{user.banned ? "✅ Yes" : "❌ No"}</td>
+          {table.getRowModel().rows.map((row) => (
+            <tr key={row.id}>
+              {row.getVisibleCells().map((cell) => (
+                <td key={cell.id}>
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </td>
+              ))}
             </tr>
           ))}
         </tbody>
--- src/hooks/useUsers.ts
+++ src/hooks/useUsers.ts
@@ -1,14 +1,8 @@
 import { useQuery } from "@tanstack/react-query";
+import type { User } from "../types/user"
 import axios from "axios";
 
 const usersUrl = "https://inqool-interview-api.vercel.app/api/users";
-
-interface User {
-  id: number;
-  name: string;
-  gender: string;
-  banned: boolean;
-}
 
 const fetchUsers = async (): Promise<User[]> => {
     const response = await axios.get(usersUrl);
