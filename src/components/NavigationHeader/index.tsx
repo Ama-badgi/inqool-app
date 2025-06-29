686cdd07c2adc40b6f7dd42a3aee9f92e6f096d4
xdobrock <xdobrock@fi.muni.cz>
2025-06-29 19:59:43 +0200

    style: layout and style the header

--- src/components/NavigationHeader/index.tsx
+++ src/components/NavigationHeader/index.tsx
@@ -1,4 +1,7 @@
 import { Link } from "react-router";
+import { TbHome } from "react-icons/tb";
+
+import "./style.css";
 
 type NavigationHeaderProps = {
   heading: string;
@@ -6,10 +9,12 @@ type NavigationHeaderProps = {
 
 function NavigationHeader({ heading }: NavigationHeaderProps) {
   return (
-    <>
-      <Link to="/">Home</Link>
-      <h1>{heading}</h1>
-    </>
+    <header className="navigation-header">
+      <Link to="/" className="navigation-header__home-link">
+        <TbHome size={28} />
+      </Link>
+      <h1 className="navigation-header__title">{heading}</h1>
+    </header>
   );
 }
 
